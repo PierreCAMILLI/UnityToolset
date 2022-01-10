4294d567dd26d989d7ee7aff6f285ae9df36772e
Pierre CAMILLI <camillipierre@gmail.com>
2022-01-10 22:55:48 +0100

    Remove example scripts for DebugSettings

--- DebugSettings/AnotherExampleDebugSettings.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class AnotherExampleDebugSettings : Toolset.DebugSetting
-{
-    public int[] _intProperties;
-}
